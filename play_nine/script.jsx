a4121fd25db85617d67ddcfa4aa3bac3b570d46f
Lukasz Siwek <xofczarx@gmail.com>
2017-07-31 15:29:37 +0200

    refactoring

--- play_nine/script.jsx
+++ play_nine/script.jsx
@@ -21,7 +21,9 @@ const Answer = (props) => {
 const Button = (props) => {
 	return (
   	<div className="col-2">
-      <button>=</button>
+      <button className="btn" disabled={props.selectedNumbers.length === 0}>
+        =
+      </button>
     </div>
   );
 }
@@ -68,18 +70,19 @@ class Game extends React.Component {
   }
   
   render() {
+  	const { selectedNumbers, randomNumberOfStars } = this.state;
   	return (
     	<div className="container">
         <h3>Play Nine</h3>
         <hr />
         <div className="row">
-          <Stars numberOfStars={this.state.randomNumberOfStars}/>
-          <Button />
-          <Answer selectedNumbers={this.state.selectedNumbers}
+          <Stars numberOfStars={randomNumberOfStars} />
+          <Button selectedNumbers={selectedNumbers} />
+          <Answer selectedNumbers={selectedNumbers}
                   unselectNumber={this.unselectNumber} />
         </div>
         <br />
-        <Numbers selectedNumbers={this.state.selectedNumbers}
+        <Numbers selectedNumbers={selectedNumbers}
                  selectNumber={this.selectNumber}/>
       </div>
     );
